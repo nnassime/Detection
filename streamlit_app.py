b9b48c3e0f36d5f4443b875d2b81970903a70951
A. Belaid <62215850+nnassime@users.noreply.github.com>
2021-12-01 22:13:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,45 @@
 import streamlit as st
 
+from mtcnn.mtcnn import MTCNN
+import matplotlib.pyplot as plt 
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
+from PIL import Image
+from numpy import asarray
+from scipy.spatial.distance import cosine
+
 import os
 
-choice = st.selectbox("Select Option",[
-    "Face Detection",
-    "Face Detection 2",
-    "Face Verification"
-])
 
-st.title("Test of Person detection")
+def head():
+    st.title("Test of Person detection")
+    st.text("")
+    st.text("")
+return
 
-st.text("")
 
-st.text("")
+def main():
+    fig = plt.figure()
+    if choice == "Face Detection":
+        uploaded_file = st.file_uploader("Choose File", type=["jpg","png"])
+        if uploaded_file is not None:
+            data = asarray(Image.open(uploaded_file))
+            plt.axis("off")
+            plt.imshow(data)
+            ax = plt.gca()
+          
+            detector = MTCNN()
+            faces = detector.detect_faces(data)
+            for face in faces:
+                x, y, width, height = face['box']
+                rect = Rectangle((x, y), width, height, fill=False, color='maroon')
+                ax.add_patch(rect)
+                for _, value in face['keypoints'].items():
+                    dot = Circle(value, radius=2, color='maroon')
+                    ax.add_patch(dot)
+              st.pyplot(fig)
+            
+            
+if __name__ == "__main__":
+    head()
+    main()
